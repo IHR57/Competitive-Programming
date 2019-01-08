c4a516991658515007a05fa958e293724ec976d4
IHR57 <iqbalhrasel95@gmail.com>
2019-01-08 18:28:42 +0600

    [Competitive-Programming] - template has been updated

--- template.cpp
+++ template.cpp
@@ -16,8 +16,15 @@
 #include <sstream>
 #include <iomanip>
 #include <list>
+#define cos(a) cos(a*pi/180)
+#define sin(a) sin(a*pi/180)
+#define tan(a) tan(a*pi/180)
+#define cosi(a) acos(a)/(pi/180)
+#define sini(a) asin(a)/(pi/180)
+#define tani(a) atan(a)/(pi/180)
 #define read (freopen("input.txt", "r", stdin))
 #define write (freopen("output.txt", "w", stdout))
+#define fastIO (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
 #define pb push_back
 #define mp make_pair
 #define PI acos(-1)
@@ -33,6 +40,7 @@ typedef vector<int> vi;
 typedef vector<string> vs;
 typedef pair<int, int> pii;
 typedef vector<pii> vii;
+typedef priority_queue<int,vector<int>,greater<int> > pq;
 
 const int inf = 1<<28;
 
