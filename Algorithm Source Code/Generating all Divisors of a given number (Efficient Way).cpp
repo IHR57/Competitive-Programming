303b014ce83f3a3b356d44feb3cf3255670b2bac
IHR57 <iqbalhrasel95@gmail.com>
2019-02-21 18:33:49 +0600

    [Competitive-Programming] - Generate all Divisors (Efficient Way)

---
+++ Algorithm Source Code/Generating all Divisors of a given number (Efficient Way).cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#define MAX 10000020
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+bool mark[MAX];
+vector<int> primes;
+vector<ll> divisors;
+vector<pii> factors;
+
+void genSieve()
+{
+    int limit = sqrt(MAX);
+    mark[1] = true;
+    for(int i = 4; i < MAX; i += 2)
+        mark[i] = true;
+
+    primes.push_back(2);
+
+    for(int i = 3; i < MAX; i += 2){
+        if(!mark[i]){
+            primes.push_back(i);
+            if(i <= limit){
+                for(int j = i * i; j < MAX; j += i * 2){
+                    mark[j] = true;
+                }
+            }
+        }
+    }
+}
+
+void primeFactors(ll n)
+{
+    int idx = 0;
+    ll pf = (ll) primes[idx], ans = 1;
+
+    while(pf * pf <= n){
+        int cnt = 0;
+        while(n % pf == 0){
+            cnt++;
+            n /= pf;
+        }
+        if(cnt > 0)
+            factors.push_back(make_pair(pf, (ll) cnt));
+        pf = (ll) primes[++idx];
+    }
+    if(n > 1){
+        factors.push_back(make_pair(n, 1));
+    }
+}
+
+void setDivisors(ll n, int i)
+{
+    ll x;
+    int j, k;
+    for(j = i; j < factors.size(); j++){
+        x = factors[j].first * n;
+        for(k = 0; k < factors[j].second; k++){
+            divisors.push_back(x);
+            setDivisors(x, j + 1);
+            x *= factors[j].first;
+        }
+    }
+}
+
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+    genSieve();
+    ll n;
+    int test, caseno = 1;
+
+    scanf("%d", &test);
+
+    while(test--){
+        scanf("%lld", &n);
+
+        factors.clear();
+        divisors.clear();
+
+        primeFactors(n);
+        setDivisors(1, 0);
+
+        divisors.push_back(1);
+        sort(divisors.begin(), divisors.end());
+        for(int i = 0; i < divisors.size(); i++){
+            cout<<divisors[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
